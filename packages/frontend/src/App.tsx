4de70614e7304144251cc0ef0ec58103a02e5a80
Alfredo Guida <guida.alfredo@gmail.com>
2022-01-31 21:37:22 +0100

    setup rough router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,27 +1,48 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import "./index.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
 
-function App() {
+function Index() {
+  return <h2>Home</h2>;
+}
+
+type TParams = { id: number };
+
+function Product(): JSX.Element {
+  const params = useParams();
+
+  return <h2>This is a page for product with ID: {params.id}</h2>;
+}
+
+function AppRouter() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className=" font-bold text-gray-500"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products/1">First Product</Link>
+            </li>
+            <li>
+              <Link to="/products/2">Second Product</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/products/:id" element={<Product />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default AppRouter;
